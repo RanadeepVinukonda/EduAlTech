1fb4e1f83d2c713d5aded131918d07030da4f751
ranadeep <viranadeep@gmail.com>
2025-07-06 16:32:51 +0530

    Edited axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,10 @@
 // src/axios.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE==="development"? "http://localhost:5000/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "https://edualtech.onrender.com/api"
+    : "/api";
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // This allows cookies to be sent with requests
