ac4b24bce601699cb684beb7619c81c9738902dd
ranadeep <viranadeep@gmail.com>
2025-07-06 16:53:30 +0530

    Linking

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api" // ⬅️ use local backend when developing
-    : "https://edualtech.onrender.com/api"; // ⬅️ production backend
+    : "/api"; // ⬅️ production backend
 
 const api = axios.create({
   baseURL: BASE_URL,
