255f0a99bfa0b13eafdec43779e487b311e28a99
ranadeep <viranadeep@gmail.com>
2025-07-26 11:30:11 +0530

    email_resolve2

--- backend/sendMail.js
+++ backend/sendMail.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, // smtp.zoho.in
-  port: Number(process.env.SMTP_PORT), // 587
-  secure: false, // false for TLS (STARTTLS)
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: process.env.SMTP_SECURE === "true",
   auth: {
-    user: process.env.SMTP_EMAIL,
+    user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
