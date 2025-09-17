4997692395d483dbe762c22c542cfb32c55b0b55
ranadeep <viranadeep@gmail.com>
2025-09-17 23:21:10 +0530

    debug10

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : "https://edualtech.onrender.com/api";
+    : "https://www.edualtech.xyz/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
