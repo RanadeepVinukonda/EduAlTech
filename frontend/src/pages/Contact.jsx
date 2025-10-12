09b955c4a4b9924bb3cf41fab8e266e44b50e737
ranadeep <viranadeep@gmail.com>
2025-10-12 14:23:55 +0530

    image1

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -32,7 +32,7 @@ const teamMembers = [
     role: "Backend Engineer",
     email: "ukkukk97@gmail.com",
     phone: "+91 9121505879",
-    image: "/assets/team/uma.jpeg",
+    image: "/team/uma.jpeg",
     linkedin:
       "https://www.linkedin.com/in/chokkapu-uma-krishna-kanth-50a502288/",
   },
